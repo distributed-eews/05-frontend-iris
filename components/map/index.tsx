c2263b7090aad332b8066f5c80af69980c77b2a1
Taufik Pragusga <taufik@pragusga.com>
2024-04-02 15:55:17 +0700

    iris

--- components/map/index.tsx
+++ components/map/index.tsx
@@ -1,31 +1,31 @@
-import { MAPBOX_TOKEN } from "@/lib/credentials";
-import React, { useRef, useCallback } from "react";
-import Map from "react-map-gl";
-import stationdata from "../stations.json";
-import { IStation } from "@/lib/interfaces/stations";
-import { Station } from "./marker";
-import type { MapRef } from "react-map-gl";
-import { getPulseDot } from "./pulse";
-import type { FeatureCollection } from "geojson";
-import { useEEWS } from "@/lib/hooks/useEEWS";
-import { EventMarker } from "./event";
+import { MAPBOX_TOKEN } from '@/lib/credentials';
+import React, { useRef, useCallback } from 'react';
+import Map from 'react-map-gl';
+import stationdata from '../stations.json';
+import { IStation } from '@/lib/interfaces/stations';
+import { Station } from './marker';
+import type { MapRef } from 'react-map-gl';
+import { getPulseDot } from './pulse';
+import type { FeatureCollection } from 'geojson';
+import { useEEWS } from '@/lib/hooks/useEEWS';
+import { EventMarker } from './event';
 
 export const MapGL: React.FC = () => {
   const [viewState, setViewState] = React.useState({
     longitude: 117,
     latitude: -3,
-    zoom: 3.8,
+    zoom: 2.0,
   });
   const { stations, event } = useEEWS();
 
   const mapRef = useRef<MapRef>(null);
   const onMapLoad = useCallback(() => {
-    mapRef.current?.addImage("pulsing-dot", getPulseDot(mapRef.current));
+    mapRef.current?.addImage('pulsing-dot', getPulseDot(mapRef.current));
   }, [mapRef]);
   return (
     <Map
       {...viewState}
-      minZoom={3.8}
+      minZoom={2.0}
       mapboxAccessToken={MAPBOX_TOKEN}
       onMove={(evt) => setViewState(evt.viewState)}
       mapStyle="mapbox://styles/mapbox/satellite-v9"
